f9a3ab845351f806beb6ec206e0fd1be56351a38
j-artur <jartur.dev@gmail.com>
2022-08-24 21:28:11 -0300

    Resolução Lab08

--- Labs/Lab08/FroggerDemo/FroggerDemo/Frogger.cpp
+++ Labs/Lab08/FroggerDemo/FroggerDemo/Frogger.cpp
@@ -1,6 +1,6 @@
 /**********************************************************************************
 // Frogger (Código Fonte)
-// 
+//
 // Criação:     19 Dez 2012
 // Atualização: 14 Ago 2021
 // Compilador:  Visual C++ 2019
@@ -15,17 +15,29 @@
 
 Frogger::Frogger()
 {
-    sprite = new Sprite("Resources/frog.png");
-    MoveTo(window->CenterX() - sprite->Width()/2.0f, 545.0f, Layer::FRONT);
+    spriteUp = new Sprite("Resources/frog_up.png");
+    spriteDown = new Sprite("Resources/frog_down.png");
+    spriteLeft = new Sprite("Resources/frog_left.png");
+    spriteRight = new Sprite("Resources/frog_right.png");
+
+    direction = UP;
+
+    MoveTo(window->CenterX(), 560.0f, Layer::FRONT);
+
     ctrlUp = true;
     ctrlDown = true;
+    ctrlLeft = true;
+    ctrlRight = true;
 }
 
 // ---------------------------------------------------------------------------------
 
 Frogger::~Frogger()
 {
-    delete sprite;
+    delete spriteUp;
+    delete spriteDown;
+    delete spriteLeft;
+    delete spriteRight;
 }
 
 // ---------------------------------------------------------------------------------
@@ -37,29 +49,79 @@ void Frogger::Update()
     {
         Translate(0.0f, -40.0f);
         ctrlUp = false;
+        direction = UP;
     }
     else if (window->KeyUp(VK_UP))
     {
         ctrlUp = true;
     }
-    
+
     // desloca sapo para baixo
     if (ctrlDown && window->KeyDown(VK_DOWN))
     {
         Translate(0.0f, 40.0f);
         ctrlDown = false;
+        direction = DOWN;
     }
     else if (window->KeyUp(VK_DOWN))
     {
         ctrlDown = true;
     }
-    
-    // mantém sapo dentro da tela
-    if (y < 65)
-        MoveTo(x, 65);
 
-    if (y > 545)
-        MoveTo(x, 545);
+    // desloca sapo para esquerda
+    if (ctrlLeft && window->KeyDown(VK_LEFT))
+    {
+        Translate(-40.0f, 0.0f);
+        ctrlLeft = false;
+        direction = LEFT;
+    }
+    else if (window->KeyUp(VK_LEFT))
+    {
+        ctrlLeft = true;
+    }
+
+    // desloca sapo para direita
+    if (ctrlRight && window->KeyDown(VK_RIGHT))
+    {
+        Translate(40.0f, 0.0f);
+        ctrlRight = false;
+        direction = RIGHT;
+    }
+    else if (window->KeyUp(VK_RIGHT))
+    {
+        ctrlRight = true;
+    }
+
+    // mantém sapo dentro da tela
+    if (x < 20)
+        MoveTo(20, y);
+    if (x > 780)
+        MoveTo(780, y);
+    if (y < 80)
+        MoveTo(x, 80);
+    if (y > 560)
+        MoveTo(x, 560);
+}
+
+// ---------------------------------------------------------------------------------
+
+inline void Frogger::Draw()
+{
+    switch (direction)
+    {
+    case UP:
+        spriteUp->Draw(x - spriteUp->Width() / 2.0f, y - spriteUp->Height() / 2.0f, z);
+        break;
+    case DOWN:
+        spriteDown->Draw(x - spriteDown->Width() / 2.0f, y - spriteDown->Height() / 2.0f, z);
+        break;
+    case LEFT:
+        spriteLeft->Draw(x - spriteLeft->Width() / 2.0f, y - spriteLeft->Height() / 2.0f, z);
+        break;
+    case RIGHT:
+        spriteRight->Draw(x - spriteRight->Width() / 2.0f, y - spriteRight->Height() / 2.0f, z);
+        break;
+    }
 }
 
 // ---------------------------------------------------------------------------------
--- Labs/Lab08/FroggerDemo/FroggerDemo/Frogger.h
+++ Labs/Lab08/FroggerDemo/FroggerDemo/Frogger.h
@@ -1,6 +1,6 @@
 /**********************************************************************************
 // Frogger (Arquivo de Cabeçalho)
-// 
+//
 // Criação:     19 Dez 2012
 // Atualização: 14 Ago 2021
 // Compilador:  Visual C++ 2019
@@ -15,32 +15,40 @@
 // ---------------------------------------------------------------------------------
 // Inclusões
 
-#include "Types.h"             // tipos específicos da engine
-#include "Object.h"            // interface de object
-#include "Sprite.h"            // interface de sprites
+#include "Object.h" // interface de object
+#include "Sprite.h" // interface de sprites
+#include "Types.h"  // tipos específicos da engine
 
 // ---------------------------------------------------------------------------------
 
+enum Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
 class Frogger : public Object
 {
-private:
-    Sprite * sprite;
+  private:
+    Sprite *spriteUp;
+    Sprite *spriteDown;
+    Sprite *spriteLeft;
+    Sprite *spriteRight;
+    Direction direction;
     bool ctrlUp;
     bool ctrlDown;
+    bool ctrlLeft;
+    bool ctrlRight;
 
-public:
+  public:
     Frogger();
     ~Frogger();
 
     void Update();
     void Draw();
 };
-
-// ---------------------------------------------------------------------------------
-// Funções Membro Inline
-
-inline void Frogger::Draw()
-{ sprite->Draw(x,y,z); }
 
 // ---------------------------------------------------------------------------------
 
